a19046d089101ce2be842335b708e5d18dea43bd
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-01-13 19:00:13 -0300

    feat(config.mk): add build configuration file
    Add file for customizing the build process.

---
+++ config.mk
@@ -0,0 +1,13 @@
+# c-alculator version
+VERSION = 1.0.0
+
+# Customization
+
+## File path
+PREFIX = /usr/local
+
+## Flags
+CFLAGS = -g3 -Wall -Wextra -Wconversion -Wdouble-promotion -Wno-unused-parameter -Wno-unused-function -Wno-sign-conversion -fsanitize=undefined -fsanitize-trap
+
+## Compiler
+CC = cc
