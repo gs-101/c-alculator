f484d3277ccd93cadab0455a5f75c8fc9397ee9d
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-01-13 19:02:54 -0300

    feat(c-alculator.h): add header file
    Add the header file of the project, which kind of serves as a library.

---
+++ c-alculator.h
@@ -0,0 +1,65 @@
+/* c-alculator --- Simple calculator using C
+
+Copyright (C) 2025 Gabriel Santos
+
+Author: Gabriel Santos
+URL: https://github.com/gs-101/c-alculator
+Mirror: https://codeberg.org/gs-101/c-alculator
+SPDX-License-Identifier: GPL-3.0-or-later
+
+ This file is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published
+ by the Free Software Foundation, either version 3 of the License
+ (at your option) any later version.
+
+ This file is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+Commentary:
+
+ Functionality for c-alculator.
+
+Code:
+ */
+
+#define NO_INPUT !fgets(buf, sizeof(buf), stdin)
+
+#define INTRO " ______ \n/ ____|\n| |     \n| |     \n| |____ \n\\_____| alculator\n\n[c]alculate [q]uit\n"
+
+#define OPERATION_MENU "[1] Addition\n[2] Subtraction\n[3] Multiplication\n[4] Division\n"
+
+#define QUIT_OPERATION "Pleasure doing calculations for you!"
+
+#define GET_NUMBERS                                                                                                    \
+    int x, y;                                                                                                          \
+    char buf[1024], *endptr;                                                                                           \
+    printf("Enter the first number:\n");                                                                               \
+    if (NO_INPUT)                                                                                                      \
+        return FAILURE;                                                                                                \
+    else                                                                                                               \
+        x = strtol(buf, &endptr, 10);                                                                                  \
+    printf("Enter the second number:\n");                                                                              \
+    if (NO_INPUT)                                                                                                      \
+        return FAILURE;                                                                                                \
+    else                                                                                                               \
+        y = strtol(buf, &endptr, 10);
+
+extern int main_menu(void);
+
+extern int operation(void);
+
+extern int addition(void);
+
+extern int subtraction(void);
+
+extern int multiplication(void);
+
+extern int division(void);
+
+#define SUCCESS 0
+#define FAILURE -1
